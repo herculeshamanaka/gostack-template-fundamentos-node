af149eee07c4a192bb3bafa620d955321aaa4b89
Hercules Neves Hamanaka <hamanhrc@schaeffler.com>
2020-04-15 10:32:41 -0300

    chore: Initial Commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,27 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface ListTransactionDTO {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionsAndBalanceDTO {
+  transactions: ListTransactionDTO[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -14,15 +32,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public getTransactions(): TransactionsAndBalanceDTO {
+    const values: TransactionsAndBalanceDTO = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return values;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(trans => trans.type === 'income')
+      .reduce((sum, trans) => {
+        return sum + trans.value;
+      }, 0);
+
+    const outcome = this.transactions
+      .filter(trans => trans.type === 'outcome')
+      .reduce((sum, trans) => {
+        return sum + trans.value;
+      }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (balance.total < value) {
+        throw Error('There is not enough balance for this transaction!');
+      }
+    }
+    const newTransaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return newTransaction;
   }
 }
 
